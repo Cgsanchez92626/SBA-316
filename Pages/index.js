f603b3a8b12ac68e2045c2787c26b1823ac4575d
Cgsanchez92626 <159073103+Cgsanchez92626@users.noreply.github.com>
2024-06-18 16:48:24 -0700

    Modified Index.js

--- Pages/index.js
+++ Pages/index.js
@@ -23,15 +23,67 @@ var menuLinks = [
   document.addEventListener("DOMContentLoaded", function () {
     // Select and cache the <main> element
     var mainEl = document.querySelector("main");
+
+
   
     // Set the background color of mainEl using CSS custom property
     mainEl.style.backgroundColor = "var(--main-bg)";
   
+    // Create the elements
+    var img = document.createElement('img');
+    var h1 = document.createElement('h1');
+    var h4 = document.createElement('h4');
+    var div = document.createElement('div');
+    var hr = document.createElement('hr');
+    var strong = document.createElement('strong');
+    var em = document.createElement('em');
+
+
+    // Set attributes
+    img.id = 'myLogo';
+    img.src = '/Images/PremierRealtyAssociates_logo jpg.jpg';
+    // img.style = 'flex-ctr';
+    img.height = '200px';
+    img.width = '300px';
+        // Append elements to main
+    // const main = document.querySelector('main');
+    // main.style.display = 'flex';
+    img.style.flexDirection = 'column';
+    img.style.alignItems = 'center'; // Center align items horizontally
+
+
+    // Set inline styles
+    img.style.display = 'flex';
+    img.style.border = '2px solid black';
+    img.style.padding = '10px';
+    img.style.backgroundColor = '#f3eded';
+    img.style.boxShadow = '10px 10px 1px rgba(117, 115, 115, 0.3)';
+    img.style.animation = 'mymove 5s infinite';
+
     // Set the content of mainEl to <h1>CSREI Group</h1>
-    mainEl.innerHTML = "<h1>CSREI Group</h1>";
+    h1.textContent = 'CSREI Group';
+    h4.textContent = 'Providing Services in Commercial And Residential Real Estate Investment guidance';
+    
+    div.id = 'services';
+    div.innerHTML = `
+    <section>Residential</section>
+    <section>Commercial</section>
+    <section>Real Estate Investing</section>
+    `;
+    strong.appendChild(em);
+    em.textContent = 'Coming Soon';
+    // Append elements to main
+    mainEl.appendChild(img);
+    mainEl.appendChild(h1);
+    mainEl.appendChild(h4);
+    mainEl.appendChild(div);
+    mainEl.appendChild(hr);
+    mainEl.appendChild(document.createElement('div')).className = 'footer';
+    mainEl.querySelector('.footer').appendChild(strong);
+
   
     // Add a class of flex-ctr to mainEl
-    mainEl.classList.add("flex-ctr");
+    // mainEl.classList.add("flex-row");
   
     // Select and cache the <nav id="top-menu"> element
     var topMenuEl = document.getElementById("top-menu");
